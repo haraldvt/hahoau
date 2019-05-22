9e1c3c813df07e5048ffcf51dad2fce6032cafbb
Harald <htvl@filternet.nl>
2019-05-22 19:08:24 +0000

    divers

--- flaskr/sunscreens.py
+++ flaskr/sunscreens.py
@@ -17,10 +17,12 @@ bp = Blueprint('sunscreens', __name__, url_prefix='/sunscreens')
 
 RELAY_SUNSCREEN = [17, 18, 27, 22]
 RELAY_MOTOR_POWER = 25
-RELAY_MOTOR_OVERLOAD = 4
+RELAY_MOTOR_OVERLOAD = 8
 
-MOTOR_1_C   = 23  # clockwise
-MOTOR_1_CC  = 24  # counterclockwise
+MOTOR_1  = 23  
+MOTOR_2  = 24
+
+KNOP_NEER= 4
 
 # Motor speed 0 (off) to 255 (max speed)
 currentMotorSpeed = 0
@@ -36,13 +38,20 @@ status_motor_power = GPIO.LOW
 GPIO.output(RELAY_MOTOR_POWER, status_motor_power)
 GPIO.setup(RELAY_MOTOR_OVERLOAD, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+GPIO.setup(KNOP_NEER, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
 # create a pigpio object, that accesses the pigpiod deamon (which handles the PWM to the motors)
 pi = pigpio.pi()
 if not pi.connected:
     exit()
-pi.set_mode(MOTOR_1_C  , pigpio.OUTPUT)
-pi.set_mode(MOTOR_1_CC , pigpio.OUTPUT)
-
+pi.set_mode(MOTOR_1  , pigpio.OUTPUT)
+pi.set_mode(MOTOR_2 , pigpio.OUTPUT)
+#pi.set_PWM_frequency(MOTOR_1, 10000)
+print(pi.get_PWM_frequency(MOTOR_1))
+print(pi.get_PWM_range(MOTOR_1))
+#pi.set_PWM_frequency(MOTOR_2, 10000)
+print(pi.get_PWM_frequency(MOTOR_2))
+print(pi.get_PWM_range(MOTOR_2))
 
 
 @bp.route('/', methods=('GET', 'POST'))
@@ -51,6 +60,17 @@ def show():
         title = request.form['title']    
         flash("boeh!")
     return render_template('sunscreens/sunscreens.html')
+
+@bp.route('/<int:screen_id>/calup', methods=('POST',))
+def calup(screen_id):
+    control(screen_id, 'calup', 100)
+    return render_template('sunscreens/sunscreens.html')
+
+@bp.route('/<int:screen_id>/caldown', methods=('POST',))
+def caldown(screen_id):
+    control(screen_id, 'caldown', 100)
+    return render_template('sunscreens/sunscreens.html')
+
 
 @bp.route('/<int:screen_id>/up', methods=('POST',))
 def up(screen_id):
@@ -62,7 +82,7 @@ def down(screen_id):
     control(screen_id, 'down', 100)
     return render_template('sunscreens/sunscreens.html')
 
-def control(screen_id, movement, percentage):
+def control(screen_id, movement, percentage, ms=5000.0):
     ensure_motor_power()
     
     ss = [screen_id]
@@ -83,7 +103,7 @@ def control(screen_id, movement, percentage):
     start_time = time.time()
     overload = GPIO.input(RELAY_MOTOR_OVERLOAD)
     print(overload)
-    while (((time.time() - start_time) < 5.0) and (overload == 0)):
+    while (((time.time() - start_time) < int(ms / 1000.0)) and (overload == 0)):
       time.sleep(0.1)
       overload = GPIO.input(RELAY_MOTOR_OVERLOAD)
       if (overload):
@@ -102,8 +122,8 @@ def control(screen_id, movement, percentage):
 def turnOffMotors():
     if not pi is None:
         print(" All off")
-        pi.set_PWM_dutycycle(MOTOR_1_C,  0)
-        pi.set_PWM_dutycycle(MOTOR_1_CC, 0)
+        pi.set_PWM_dutycycle(MOTOR_1,  0)
+        pi.set_PWM_dutycycle(MOTOR_2, 0)
 
 atexit.register(turnOffMotors)
 
@@ -118,19 +138,19 @@ def motor_event(newSpeed):
     
     for speed in speedrange:
       if speed == 0:
-          pi.set_PWM_dutycycle(MOTOR_1_C,  0)
-          pi.set_PWM_dutycycle(MOTOR_1_CC, 0)
+          pi.set_PWM_dutycycle(MOTOR_1,  0)
+          pi.set_PWM_dutycycle(MOTOR_2, 0)
       elif speed > 0:
-          pi.set_PWM_dutycycle(MOTOR_1_C,  speed)
-          pi.set_PWM_dutycycle(MOTOR_1_CC, 0)
+          pi.set_PWM_dutycycle(MOTOR_1,  speed)
+          pi.set_PWM_dutycycle(MOTOR_2, 0)
       else:
-          pi.set_PWM_dutycycle(MOTOR_1_C,  0)
-          pi.set_PWM_dutycycle(MOTOR_1_CC, abs(speed))
+          pi.set_PWM_dutycycle(MOTOR_1,  0)
+          pi.set_PWM_dutycycle(MOTOR_2, abs(speed))
       time.sleep(0.01)
 
     if newSpeed == 0:
-        pi.set_PWM_dutycycle(MOTOR_1_C,  0)
-        pi.set_PWM_dutycycle(MOTOR_1_CC, 0)
+        pi.set_PWM_dutycycle(MOTOR_1,  0)
+        pi.set_PWM_dutycycle(MOTOR_2, 0)
 
     currentMotorSpeed = newSpeed
     print("Motor speed: ", currentMotorSpeed)
